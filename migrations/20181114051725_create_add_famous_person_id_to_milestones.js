0b93156964d82924bed18ed3b24cc3fd0d9c1611
Rachel Wong <rwrachelwong@gmail.com>
2018-11-14 05:46:16 +0000

    Add foreign key - famous_person_id

---
+++ migrations/20181114051725_create_add_famous_person_id_to_milestones.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('milestones', function(table) {
+      table.integer('famous_person_id');
+      table.foreign('famous_person_id').references('famous_people.id');
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropColumn('famous_person_id');
+};
